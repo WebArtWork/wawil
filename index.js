f7da55b175ecb15c4f9475817babcf63a5b92a22
NastyaVod <104510101+NastyaVod@users.noreply.github.com>
2022-07-07 15:02:35 +0300

    post request

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 #!/usr/bin/env node
 const argv = process.argv.slice();
+argv.splice(0,2);
+
+async function postData(url = '', data = []) {
+    const response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        redirect: 'follow', 
+        referrerPolicy: 'no-referrer', 
+        body: data
+    });
+    return response;
+}
+console.log(postData('http://localhost:8080/api/illustration/list', argv));
+
 console.log(argv);
 process.exit(1);
