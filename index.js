ec4ce22953d5795b89631dfc3ea41aff027bc723
NastyaVod <anastasiya.alecs.vodneva@gmail.com>
2022-07-07 19:43:45 +0300

    search for icon.module.ts

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 #!/usr/bin/env node
+const fs = require('fs');
+function getfiles (dir, files){
+	files = files || [];
+	var allFiles = fs.readdirSync(dir);
+	for (var i =0; i<allFiles.length; i++){
+		var name = dir + '/' + allFiles[i];
+		if (fs.statSync(name).isDirectory()){
+			getfiles (name, files);
+		} else {
+			  files.push(name);
+		}
+	}
+	return files;
+};
+if (getfiles (".//").indexOf(".//src") == -1) {
+	console.log("Function not found");
+	process.exit(1);
+}
+if (getfiles (".//src/app/modules/").indexOf(".//src/app/modules//icons/icons.module.ts") == -1) {
+	console.log("No icons module");
+	process.exit(1);
+}
+
 const axios = require('axios').default;
 const argv = process.argv.slice();
 argv.splice(0,2);
